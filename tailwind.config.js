778d32143ae1d6b9895782a58150f90fd2073cff
mhafet17 <do.crazy192@gmail.com>
2020-05-27 20:59:45 +0700

    generate config taiwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    opacity: ["responsive", "hover", "focus", "disabled"],
+    textColor: [
+      "responsive",
+      "hover",
+      "focus",
+      "visited",
+      "group-hover",
+      "group-focus",
+      "focus-within",
+    ],
+  },
+  plugins: [],
+};
